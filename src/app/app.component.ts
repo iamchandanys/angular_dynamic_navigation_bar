39c0c98b5f66ba107cebaa7e992f5d9805c0dd77
imchandanys <chandanys18@gmail.com>
2019-07-08 14:27:01 +0530

    6 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { NavBarData } from './dashboard-menus';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+
+  constructor(){
+    this.GetNavArray();
+  }
+
+  dashboardMainMenu: any[] = [];
+  sep: any;
+  AppPermissions: any[] = ["ET Search", "WF View"];
+  menuArr: MainArrayType[] = [];
+
+  GetNavArray() {
+    this.dashboardMainMenu = NavBarData;
+    this.sep = this.dashboardMainMenu.length - 2;
+
+    for (let schild of this.dashboardMainMenu) {
+      this.menuArr.push(schild.child);
+
+      if (schild.child && schild.child.length > 0) {
+        schild.child.forEach((x: any) => {
+          if (this.AppPermissions.includes(x.Permission)) {
+            x.Visible = "true";
+            schild.Visible = "true";
+          }
+        });
+      }
+
+      if (this.AppPermissions.includes(schild.Permission)) {
+        schild.Visible = "true";
+      }
+    }
+  }
+}
+
+export class MainArrayType {
+  applicationName: string;
+  url: string;
 }
---
+++ src/app/dashboard-menus.ts
@@ -0,0 +1,367 @@
+export const NavBarData = [
+  {
+    "applicationName": "Assets & Titles",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "Asset Search",
+        "url": "elementtracking/searchassets",
+        "Visible": "false",
+        "Permission": "ET Search",
+        "subchild": []
+      },
+      {
+        "applicationName": "Title Search",
+        "url": "assetmanagement",
+        "Visible": "false",
+        "Permission": "Title View",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "Order Manager",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "Workflow Monitor",
+        "url": "workflowmonitor",
+        "Visible": "false",
+        "Permission": "WF View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Workflow Templates",
+        "url": "workflowtemplates",
+        "Visible": "false",
+        "Permission": "WF View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Bulk Action",
+        "url": "workflowbulkaction",
+        "Visible": "false",
+        "Permission": "WF View",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "Automation Dashboard",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "All Tasks",
+        "url": "automationdashboard/alltasks",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "My Tasks",
+        "url": "automationdashboard/mytasks",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Task Types",
+        "url": "automationdashboard/tasktypes",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Setup Groups",
+        "url": "automationdashboard/setupgroups",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Setup Users",
+        "url": "automationdashboard/setupusers",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Machine Configuration",
+        "url": "automationdashboard/machineconfiguration",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Processors",
+        "url": "automationdashboard/processors",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Tokens",
+        "url": "automationdashboard/tokens",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Retry Tasks",
+        "url": "automationdashboard/retrytasks",
+        "Visible": "false",
+        "Permission": "TS Admin",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "Transfer Manager",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "Transfer Request Status",
+        "url": "tmconfiguration/status",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Device",
+        "url": "tmconfiguration/device",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Device Endpoints",
+        "url": "tmconfiguration/deviceendpoint",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Endpoints",
+        "url": "tmconfiguration/endpoint",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Create Path",
+        "url": "tmconfiguration/createpath",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "View Paths",
+        "url": "tmconfiguration/viewpath",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "View Profiles",
+        "url": "tmconfiguration/transferprofile",
+        "Visible": "false",
+        "Permission": "TM Admin",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "Folder Monitor",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "File Event",
+        "url": "foldermonitor/fileevent",
+        "Visible": "false",
+        "Permission": "Folder View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Detected File",
+        "url": "foldermonitor/detectedfile",
+        "Visible": "false",
+        "Permission": "Folder View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Machine Setup",
+        "url": "foldermonitor/machine",
+        "Visible": "false",
+        "Permission": "Folder View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Folder Setting Setup",
+        "url": "foldermonitor/foldersetting",
+        "Visible": "false",
+        "Permission": "Folder View",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "CDN Monitor",
+    "url": "cdnmanager/productsearch",
+    "Visible": "false",
+    "Permission": "CDN View"
+  },
+  {
+    "applicationName": "Metadata",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "Default",
+        "url": "metadatadeviceweb/default",
+        "Visible": "false",
+        "Permission": "Metadata Admin",
+        "subchild": []
+      },
+      {
+        "applicationName": "Model Type",
+        "url": "metadatadeviceweb/modeltypes",
+        "Visible": "false",
+        "Permission": "Metadata Admin",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "Query Viewer",
+    "url": "queryviewer",
+    "Visible": "false",
+    "Permission": "QueryViewer"
+  },
+  {
+    "applicationName": "Bulk Order",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "Operator Console",
+        "url": "bulkorderprocessing",
+        "Visible": "false",
+        "Permission": "Bulk Order Operator",
+        "subchild": []
+      }
+    ]
+  },
+  {
+    "applicationName": "Administration",
+    "icon": "fa fa-caret-down",
+    "Visible": "false",
+    "child": [
+      {
+        "applicationName": "Manage User",
+        "url": "bulkorderprocessing",
+        "Visible": "false",
+        "Permission": "User Permission View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Locations",
+        "url": "bulkorderprocessing",
+        "Visible": "false",
+        "Permission": "ET Location",
+        "subchild": []
+      },
+      {
+        "applicationName": "Templates",
+        "url": "bulkorderprocessing",
+        "Visible": "false",
+        "Permission": "ET Template View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Title Mapping",
+        "url": "bulkorderprocessing",
+        "Visible": "false",
+        "Permission": "Title View",
+        "subchild": []
+      },
+      {
+        "applicationName": "Notification",
+        "subMenuIcon": "fa fa-caret-right",
+        "Permission": "Notification View",
+        "subchild": [
+          {
+            "applicationName": "Template",
+            "url": "notification/template",
+            "Visible": "false",
+            "Permission": "Notification View",
+            "subchild": []
+          },
+          {
+            "applicationName": "Configuration",
+            "url": "notification/configsetting",
+            "Visible": "false",
+            "Permission": "Notification View",
+            "subchild": []
+          }
+        ],
+        "child": []
+      },
+      {
+        "applicationName": "Health Monitor",
+        "subMenuIcon": "fa fa-caret-right",
+        "Permission": "abc7",
+        "subchild": [
+          {
+            "applicationName": "Configuration",
+            "url": "healthmonitor/config",
+            "Visible": "false",
+            "Permission": "abc8",
+            "subchild": []
+          },
+          {
+            "applicationName": "Health Monitor",
+            "url": "healthmonitor",
+            "Visible": "false",
+            "Permission": "abc9",
+            "subchild": []
+          }
+        ],
+        "child": []
+      },
+      {
+        "applicationName": "Bulk Order Processing",
+        "subMenuIcon": "fa fa-caret-right",
+        "Permission": "Bulk Order Operator",
+        "subchild": [
+          {
+            "applicationName": "Details",
+            "url": "bulkorderprocessing/detils",
+            "Visible": "false",
+            "Permission": "Bulk Order Operator",
+            "subchild": []
+          },
+          {
+            "applicationName": "Configuration",
+            "url": "bulkorderprocessing/client",
+            "Visible": "false",
+            "Permission": "Bulk Order Operator",
+            "subchild": []
+          }
+        ],
+        "child": []
+      },
+      {
+        "applicationName": "Upload & Download Files",
+        "url": "uploadanddownloadfiles",
+        "Visible": "false",
+        "Permission": "Bulk Order Operator",
+        "subchild": []
+      }
+    ]
+  }
+]
